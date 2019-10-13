3f166de930b40c39f6ac2bf5d57e1fc878320f63
Michael Milewski <saramic@gmail.com>
2019-10-13 17:20:58 +1100

    random notifications for annoyance
    make n
    
    can have notification, title, subtitle and sound but NO image :(
      https://alvinalexander.com/mac-os-x/how-to-fire-mac-notifications-applescript-scala-java#applescript-mac-notification-with-a-sound

---
+++ bin/notifications.rb
@@ -0,0 +1,27 @@
+#!/usr/bin/env ruby
+
+while true
+  notification = [
+    "this is getting real",
+    "time for a break",
+    "you got email",
+  ].sample
+  title = %w[Bitcoin Drinks Cake Slack].sample
+  subtitle = ["up 🔼", "down 🔽", "drinks 🍺🍹🍷🍸"].sample
+  sound = %w[Basso Blow Frog Funk Glass Hero Ping Purr Sosumi Submarine].sample
+
+  command = [
+    "display notification",
+    "\"#{notification}\"",
+    "with title",
+    "\"#{title}\"",
+    "subtitle",
+    "\"#{subtitle}\"",
+    "sound name",
+    "\"#{sound}\"",
+  ].join(" ")
+
+  system "osascript -e '#{command}'"
+
+  sleep rand(5..15)
+end
